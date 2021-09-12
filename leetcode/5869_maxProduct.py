5c000bbfcb67eedc9419baf718eae4daa438162e
pengyuan.li <pengyuan.li@tongdun.net>
2021-09-13 00:41:14 +0800

    bit dp

--- leetcode/5869_maxProduct.py
+++ leetcode/5869_maxProduct.py
@@ -42,9 +42,56 @@ class Solution:
         dfs(s, "", "", 0)
         return self.res
 
+    def maxProduct2(self, s: str) -> int:
+        n = len(s)
+        total = []  # 存放所有回文子序列
+        for i in range(1, 1 << n):  # 用位运算列出所有子序列
+            temp = ''
+            for j in range(n):
+                if (1 << j) & i:
+                    temp += s[j]
+            if temp == temp[::-1]:  # 判断是否为回文
+                total.append(i)
+
+        def count1(m: int):  # 统计二进制中1的个数
+            res = 0
+            while m:
+                res += (m & 1)
+                m >>= 1
+            return res
+
+        ret = 0
+        for i in range(len(total)):
+            for j in range(i + 1, len(total)):
+                if total[i] & total[j] == 0:  # total[i]&total[j]为0说明俩子序列不相交
+                    ret = max(ret, count1(total[i]) * count1(total[j]))
+        return ret  # 1的个数表示回文序列的长度，并计算俩长度的乘积
+
+    def maxProduct3(self, s: str) -> int:
+        n = len(s)
+        total = []  # 存放所有回文子序列
+        for i in range(1, 1 << n):  # 用位运算列出所有子序列
+            temp = ''
+            for j in range(n):
+                if (1 << j) & i:
+                    temp += s[j]
+            if temp == temp[::-1]:  # 判断是否为回文
+                total.append((i, len(temp)))
+
+        ret = 0
+        for i in range(len(total)):
+            kx, x = total[i]
+            for j in range(i + 1, len(total)):
+                ky, y = total[j]
+                if kx & ky:
+                    continue
+                ret = max(ret, x * y)
+        return ret
+
 
 if __name__ == "__main__":
     s = "leetcodecom"
     instanceSol = Solution()
-    res = instanceSol.maxProduct(s)
-    print(res)
+    print(instanceSol.maxProduct(s))
+    print(instanceSol.maxProduct2(s))
+    print(instanceSol.maxProduct3(s))
