86c6a6167b1153cba80b6ed7887b0493d3ace8eb
pengyuan.li <pengyuan.li@tongdun.net>
2021-09-15 22:50:04 +0800

    delete nosense

--- algo-introduction/35.2SmallestTeam.py
+++ algo-introduction/35.2SmallestTeam.py
@@ -34,51 +34,6 @@ def smallestSufficientTeam(req_skills: List[str], people: List[List[str]]) -> Li
 
 
 if __name__ == "__main__":
-    # req_skills = ["java", "nodejs", "reactjs"]
-    # people = [["java"], ["nodejs"], ["nodejs", "reactjs"]]
-    # print(smallestSufficientTeam(req_skills, people))
-
-    # n = 10
-    # res = list()
-    # cur = [x for x in range(2, n + 1)]
-    # typeDict = {x: 0 for x in cur}
-    # # typeDict[2] = 1
-    # while True:
-    #     oneVal = [k for k, v in typeDict.items() if v == 0]
-    #     if len(oneVal)==0:
-    #         break
-    #     minPrime = min(oneVal)
-    #     res.append(minPrime)
-    #     for i in range(2, n + 1):
-    #         if i % minPrime == 0:
-    #             typeDict[i] = 1
-    #     typeDict[minPrime] = 1
-    #     # if all(typeDict)==2:
-    #     #     break
-    #
-    #
-    # print(res)
-    label = 14
-    row = 1
-    rowStart = 1
-    while rowStart*2 <= label:
-        row += 1
-        rowStart = 2 * rowStart
-    print(row, rowStart)
-    # row = row-1
-
-    def getReverse(label, row):
-        return (1 << row) + (1 << row - 1) - label - 1
-    list.sort()
-
-    if row % 2 == 0:
-        label = getReverse(label, row)
-    path = []
-    while row > 0:
-        if row % 2 == 0:
-            path.append(getReverse(label, row))
-        else:
-            path.append(label)
-        row -= 1
-        label >>= 1
-    print(path)
+    req_skills = ["java", "nodejs", "reactjs"]
+    people = [["java"], ["nodejs"], ["nodejs", "reactjs"]]
+    print(smallestSufficientTeam(req_skills, people))
