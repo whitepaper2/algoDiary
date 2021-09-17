28e98262c835b38624a2202442388d060b50e6cc
pengyuan.li <pengyuan.li@tongdun.net>
2021-09-17 17:31:32 +0800

    deep learn minStack

--- leetcode/155_min_stack.py
+++ leetcode/155_min_stack.py
@@ -81,6 +81,49 @@ class MinStack2:
         return self.val2[-1]
 
 
+"""
+思路比上面更简单：
+1、原始数据和最小点数据存储大小相同
+2、取数据之前判断数据是否存在
+总之，与一年前相比进步比较明显，还是要多动手写代码和思考
+"""
+
+
+class Stack(object):
+    def __init__(self):
+        self.rawStack = list()
+        self.minStack = list()
+
+    def push(self, x):
+        self.rawStack.append(x)
+        if self.minStack:
+            if self.minStack[-1] < x:
+                self.minStack.append(self.minStack[-1])
+            else:
+                self.minStack.append(x)
+        else:
+            self.minStack.append(x)
+
+    def pop(self):
+        if self.rawStack:
+            self.rawStack.pop()
+            self.minStack.pop()
+        else:
+            print("error stack is null")
+
+    def top(self):
+        if self.rawStack:
+            return self.rawStack[-1]
+        else:
+            print("error stack is null")
+
+    def getMin(self):
+        if self.minStack:
+            return self.minStack[-1]
+        else:
+            print("error stack is null")
+
+
 if __name__ == "__main__":
     # obj = MinStack()
     # obj.push(x)
@@ -88,6 +131,15 @@ if __name__ == "__main__":
     # param_3 = obj.top()
     # param_4 = obj.getMin()
     min_stack = MinStack2()
+    min_stack.push(-2)
+    min_stack.push(0)
+    min_stack.push(-3)
+    print(min_stack.getMin())
+    min_stack.pop()
+    print(min_stack.top())
+    print(min_stack.getMin())
+
+    min_stack = Stack()
     min_stack.push(-2)
     min_stack.push(0)
     min_stack.push(-3)
