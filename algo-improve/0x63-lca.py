15f338deb5aa02d94bc15b9a32c637ab941b29cd
pengyuan.li <pengyuan.li@tongdun.net>
2021-11-23 19:33:05 +0800

    tarjan

--- algo-improve/0x63-lca.py
+++ algo-improve/0x63-lca.py
@@ -79,6 +79,70 @@ def getLCAByIncreament(outEdges, queries):
         print('dist({},{})={}'.format(x, y, dist[x] + dist[y] - 2 * dist[c]))
 
 
+def getLCAByTarjan(outEdges, queries):
+    """
+    tarjan方法求查询组的距离，离线算法，一次性查询所有值
+    :param outEdges:
+    :param queries:
+    :return:
+    """
+    adj = defaultdict(list)
+    vertices = set()
+    for u, v in outEdges:
+        adj[u].append(v)
+        vertices.add(u)
+        vertices.add(v)
+    n = len(vertices)
+    visited = [0] * (n + 1)
+    dist = [0] * (n + 1)
+    parent = [i for i in range(n + 1)]
+    query = defaultdict(list)
+    query_id = defaultdict(list)
+
+    m = len(queries)
+    res = [0] * (m + 1)
+
+    def add_query(x, y, i):
+        query[x].append(y)
+        query_id[x].append(i)
+        query[y].append(x)
+        query_id[y].append(i)
+
+    for i in range(m):
+        x, y = queries[i][0], queries[i][1]
+        if x == y:
+            res[i + 1] = 0
+        else:
+            add_query(x, y, i + 1)
+            res[i + 1] = 1 << 30
+
+    def get(x):
+        if x == parent[x]:
+            return x
+        parent[x] = get(parent[x])
+        return parent[x]
+
+    def tarjan(x):
+        visited[x] = 1
+        for y in adj[x]:
+            if visited[y]:
+                continue
+            dist[y] = dist[x] + 1
+            tarjan(y)
+            parent[y] = x
+        for i in range(len(query[x])):
+            y = query[x][i]
+            id = query_id[x][i]
+            if visited[y] == 2:
+                lca = get(y)
+                res[id] = min(res[id], dist[x] + dist[y] - 2 * dist[lca])
+        visited[x] = 2
+
+    tarjan(1)
+    for i in range(1, m + 1):
+        print(res[i])
+
+
 if __name__ == "__main__":
     """
     有根树，根节点是1
@@ -86,3 +150,4 @@ if __name__ == "__main__":
     outEdges = [[1, 2], [1, 3], [1, 4], [2, 5], [2, 6], [4, 7], [4, 8], [7, 9]]
     queries = [[7, 2], [6, 9], [1, 2], [9, 8], [7, 9]]
     getLCAByIncreament(outEdges, queries)
+    getLCAByTarjan(outEdges, queries)
