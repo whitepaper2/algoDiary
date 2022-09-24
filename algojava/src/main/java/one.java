f04f2d5c2227cf50f222abb41c6e304d43e9059b
haoyuhua <haoyuhua@haoyuhuas-MacBook-Air.local>
2022-09-24 23:38:14 +0800

    java first commit

---
+++ algojava/src/main/java/one.java
@@ -0,0 +1,9 @@
+package algojava.src.main.java;
+
+public class one {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.print(1+2);
+    }
+    
+}
